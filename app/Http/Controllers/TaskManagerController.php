4d7eefe1c9aa0ba7e3b250ab47e8a9f95b5719f3
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-02-15 14:54:54 +0700

    BC2_SS15_Baitap2

--- app/Http/Controllers/TaskManagerController.php
+++ app/Http/Controllers/TaskManagerController.php
@@ -6,8 +6,40 @@ use Illuminate\Http\Request;
 
 class TaskManagerController extends Controller
 {
-    public function getId(Request $request){
+    public function getId(Request $request)
+    {
         $id = $request->id;
+    //lấy id của sản phẩm
+        return $id;
     }
 
+    public function store($id)
+    {
+    // lưu trữ id
+    }
+
+    public function showAll()
+    {
+    // hiển thị toàn bộ sp
+    }
+
+    public function showDetail($id)
+    {
+    // hiển thị chi tiết sp
+    }
+
+    public function edit($id)
+    {
+    // sửa sp
+    }
+
+    public function update($id)
+    {
+        // thêm sp
+    }
+
+    public function delete($id)
+    {
+        // xóa sp
+    }
 }
